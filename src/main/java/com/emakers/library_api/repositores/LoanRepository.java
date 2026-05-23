61b0baced19bb0f565cfb35e269bc0786fd3715b
Marco Tulio <marcotfrancosilva@gmail.com>
2026-05-23 16:31:24 -0300

    update: adiciona busca nos repositorios

--- src/main/java/com/emakers/library_api/repositores/LoanRepository.java
+++ src/main/java/com/emakers/library_api/repositores/LoanRepository.java
@@ -3,7 +3,9 @@ package com.emakers.library_api.repositores;
 import com.emakers.library_api.models.LoanModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface LoanRepository extends JpaRepository<LoanModel, UUID> {
+    Optional<LoanModel> findByIdAndActiveTrue(UUID id);
 }
--- src/main/java/com/emakers/library_api/repositores/PersonRepository.java
+++ src/main/java/com/emakers/library_api/repositores/PersonRepository.java
@@ -1,9 +1,11 @@
 package com.emakers.library_api.repositores;
 
 import com.emakers.library_api.models.PersonModel;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface PersonRepository extends JpaRepository<PersonModel, UUID> {
+    PersonModel findByCpf(@NotBlank String cpf);
 }
