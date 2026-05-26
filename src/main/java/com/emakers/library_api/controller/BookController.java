5767993739ffdab70ffb2b5fbb056128c9f24883
Marco Tulio <marcotfrancosilva@gmail.com>
2026-05-26 14:04:37 -0300

    update: adiciona documentacao do swagger no BookController

--- src/main/java/com/emakers/library_api/controller/BookController.java
+++ src/main/java/com/emakers/library_api/controller/BookController.java
@@ -3,6 +3,8 @@ package com.emakers.library_api.controller;
 import com.emakers.library_api.dto.BookRecordDto;
 import com.emakers.library_api.models.BookModel;
 import com.emakers.library_api.repositores.BookRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,22 +16,29 @@ import java.util.Optional;
 import java.util.UUID;
 
 @RestController()
+@Tag(name = "Books", description = "Endpoints para gerenciamento de livros")
 public class BookController {
 
     @Autowired
     BookRepository bookRepository;
 
+    @Operation(summary = "Cadastra um novo livro", description = "Salva as informações de um novo livro" +
+            " no banco de dados.")
     @PostMapping("/books")
     public ResponseEntity<BookModel> saveBook(@RequestBody @Valid BookRecordDto bookRecordDto) {
         var bookModel = new BookModel(bookRecordDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(bookRepository.save(bookModel));
     }
 
+    @Operation(summary = "Lista todos os livros", description = "Retorna uma lista contendo todos os" +
+            " livros cadastrados.")
     @GetMapping("/books")
     public ResponseEntity<List<BookModel>> getAllBooks() {
         return ResponseEntity.status(HttpStatus.OK).body(bookRepository.findAll());
     }
 
+    @Operation(summary = "Busca um livro pelo ID", description = "Retorna os detalhes de um livro específico" +
+            " utilizando o seu UUID.")
     @GetMapping("/books/{id}")
     public ResponseEntity<Object> getOneBook(@PathVariable(value="id")UUID id){
         Optional<BookModel> bookModel = bookRepository.findById(id);
@@ -39,6 +48,8 @@ public class BookController {
         return ResponseEntity.status(HttpStatus.OK).body(bookModel.get());
     }
 
+    @Operation(summary = "Atualiza um livro", description = "Atualiza as informações de um livro existente com base" +
+            " no ID fornecido.")
     @PutMapping("/books/{id}")
     public ResponseEntity<Object> updateBook(@PathVariable(value="id")UUID id,
                                              @RequestBody @Valid BookRecordDto bookRecordDto){
@@ -51,6 +62,7 @@ public class BookController {
         return ResponseEntity.status(HttpStatus.OK).body(bookRepository.save(bookUpdate));
     }
 
+    @Operation(summary = "Deleta um livro", description = "Remove permanentemente um livro do banco de dados pelo seu ID.")
     @DeleteMapping("/books/{id}")
     public ResponseEntity<Object> deleteBook(@PathVariable(value="id")UUID id){
         Optional<BookModel> bookModel = bookRepository.findById(id);
@@ -60,5 +72,4 @@ public class BookController {
         bookRepository.delete(bookModel.get());
         return ResponseEntity.status(HttpStatus.OK).body("Book Deleted");
     }
-
 }
